97fd613424c806c7826a1360de9d41e8db70dd63
Madshyson <lavanant.martin@gmail.com>
2021-11-17 23:09:35 +0100

    Everything is setup to start dev

--- botApp.js
+++ botApp.js
@@ -5,7 +5,8 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    currentChannel = client.channels.cache.get(channelId);
-    client.user.setActivity('');
+    //currentChannel = client.channels.cache.get(channelId);
+    client.user.setActivity('Who asked ?');
 });
+
+client.login(token);
